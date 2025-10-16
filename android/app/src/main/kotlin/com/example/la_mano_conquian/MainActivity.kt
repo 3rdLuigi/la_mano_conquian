5ab482cd7253d314f7646f514956226cb2e674ee
Luis Garibay <luisgaribayiii@gmail.com>
2025-10-15 18:40:14 -0700

    Initial commit, created the the flutter project,  - Created a package for game logic  - Added the package to the dependencies  - Will focus on android development then port to  ios

---
+++ android/app/src/main/kotlin/com/example/la_mano_conquian/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.la_mano_conquian
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ packages/game_logic/example/game_logic_example.dart
@@ -0,0 +1,6 @@
+import 'package:game_logic/game_logic.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
